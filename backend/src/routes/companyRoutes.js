b6ba49933f8840e5cae8d675584aaa423d91a7a4
AhamedYusuf <xhxmed.yxsxf.15@gmail.com>
2026-04-28 21:14:03 +0530

    Completed Company Backend

---
+++ backend/src/routes/companyRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getCompanies,
+  getCompanyById,
+  createCompany,
+  updateCompany,
+  deleteCompany
+} = require("../controllers/companyController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+// Protect ALL routes
+router.use(protect);
+
+//  GET all companies
+router.get("/", getCompanies);
+
+//  GET single company
+router.get("/:id", getCompanyById);
+
+//  CREATE company
+router.post("/", createCompany);
+
+//  UPDATE company
+router.put("/:id", updateCompany);
+
+//  DELETE company
+router.delete("/:id", deleteCompany);
+
+module.exports = router;

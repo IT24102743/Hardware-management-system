0e3d95c78c03ca5a3b4681154376f35d819b28e5
W S Vidarshana <supunvidarshana2004@gmail.com>
2026-05-01 18:40:00 +0530

    created controllers, model classes

---
+++ backend/src/models/customer.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Counter = require("./counter");
+
+const customerSchema = new mongoose.Schema(
+  {
+    customerId: {
+      type: Number,
+      unique: true
+    },
+
+    customerName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    customerContactNumber: {
+      type: String,
+      trim: true,
+      unique: true
+    },
+
+    customerEmail: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true
+    }
+  },
+  { timestamps: true }
+);
+
+// SAFE AUTO-INCREMENT
+customerSchema.pre("save", async function () {
+  if (this.isNew) {
+    const counter = await Counter.findOneAndUpdate(
+      { _id: "customerId" },
+      { $inc: { sequenceValue: 1 } },
+      { returnDocument: "after", upsert: true }
+    );
+
+    this.customerId = counter.sequenceValue;
+  }
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
